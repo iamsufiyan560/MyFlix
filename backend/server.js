2b4b2a9dcbf09fb6586a52b985fd2f972d0bb806
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-07-20 23:21:22 +0530

    movies api using tmdb service

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { ENV_VARS } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import movieRoutes from "./routes/movie.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
