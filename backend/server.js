ec05721c5299a27410c7749f6279d7497a5f0caf
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-07-21 22:28:18 +0530

    tv shows api

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
+import tvRoutes from "./routes/tv.route.js";
+import { protectRoute } from "./middleware/protectRoute.js";
 
 const app = express();
 
@@ -13,7 +15,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
+app.use("/api/v1/movie", protectRoute, movieRoutes);
+app.use("/api/v1/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
