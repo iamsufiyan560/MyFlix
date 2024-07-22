fd46efd6dbc51a826cdcc679e559f4e2a9f02dfd
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-07-22 23:15:04 +0530

    Search routes + backend completed

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import movieRoutes from "./routes/movie.route.js";
 import tvRoutes from "./routes/tv.route.js";
 import { protectRoute } from "./middleware/protectRoute.js";
+import searchRoutes from "./routes/search.route.js";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
