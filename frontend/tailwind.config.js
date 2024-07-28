3f0e792209b5e1994d66b57460a171e7b7486687
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-07-28 21:13:37 +0530

    feat: Add home-page & Movie-Slider

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindScrollbarHide from "tailwind-scrollbar-hide";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
