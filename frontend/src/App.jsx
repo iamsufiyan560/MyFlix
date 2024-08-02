4665598d7b83c6ad00dc6cc4c7a7cf218f188e24
iamsufiyan560 <140635114+iamsufiyan560@users.noreply.github.com>
2024-08-02 16:48:32 +0530

    feat: add SearchHistory and 404 & Complete the Proeject

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/404";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -49,6 +51,11 @@ function App() {
           path="/search"
           element={user ? <SearchPage /> : <Navigate to={"/login"} />}
         />
+        <Route
+          path="/history"
+          element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />}
+        />
+        <Route path="/*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
       <Toaster />
